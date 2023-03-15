d617beb5bf120e3d2914848372c20781b32f2afc
GunnarEngland <gunnare98@live.no>
2023-03-15 12:13:46 +0100

    Reintroduced the if hub_height == h statement

--- wind_height.py
+++ wind_height.py
@@ -12,9 +12,9 @@ def c_height(wind, hub_height):
         #    h_changed = (log10(hub_height / z0) / log10(h_0 / z0)) * wind_0[x]
         #print(f'Height lower than reference, reference height set to 0')
     #    return h_changed, False
-    #elif hub_height == h:  # Skip calculating the same wind speed.
-    #    print('Height is the same, returned input.')
-    #    return wind, True
+    if hub_height == h:  # Skip calculating the same wind speed.
+        print('Height is the same, returned input.')
+        return wind, True
     for x in wind:
         h_changed = (log10(hub_height / z0) / log10(h / z0)) * wind[x]
     print('Height for wind changed to ' + str(hub_height))
